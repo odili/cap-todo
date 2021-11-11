ecdea2e4bca936ad2bd8d7afd611151b1a95791d
Chikaodili Chukwunta <odili@users.noreply.github.com>
2021-11-11 13:46:10 +0100

    Added create todo notification message

--- client/src/component/CreateTodo.tsx
+++ client/src/component/CreateTodo.tsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { useAcessToken } from 'hooks/useAccessToken';
 import { apiEndpoint } from '../config';
 import { Todo } from 'types';
+import Notification from './Notification';
 
 type Prop = {
   todos: Todo[];
@@ -18,6 +19,10 @@ export default function CreateTodo({ todos, setTodos }: Prop) {
   const [dueDate, setDueDate] = useState<string | null>(
     new Date().toISOString()
   );
+  const [severity, setSeverity] = useState<
+    'info' | 'error' | 'warning' | 'success' | undefined
+  >(undefined);
+  const [message, setMessage] = useState('');
   const accessToken = useAcessToken();
   const apiUrl = `${apiEndpoint}/todos`;
 
@@ -31,47 +36,58 @@ export default function CreateTodo({ todos, setTodos }: Prop) {
 
   const createTodo = async (e: React.FormEvent) => {
     e.preventDefault();
-    const res = await axios({
-      url: apiUrl,
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      },
-      data: { name, dueDate }
-    });
-    // console.log(res.data);
-    setTodos([...todos, res.data?.item]);
-    setName('');
-    setDueDate(new Date().toISOString());
+    try {
+      const res = await axios({
+        url: apiUrl,
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        },
+        data: { name, dueDate }
+      });
+      // console.log(res.data);
+      setTodos([...todos, res.data?.item]);
+      setName('');
+      setDueDate(new Date().toISOString());
+      setSeverity('success');
+      setMessage(`${name} Created Successfully!`);
+    } catch (e) {
+      console.log((e as Error).message);
+      setSeverity('error');
+      setMessage(`${name} Creation Failed!`);
+    }
   };
   return (
-    <Box
-      component="form"
-      sx={{
-        display: 'grid',
-        gridTemplateColumns: '2fr 1fr auto',
-        columnGap: '0.625rem',
-        mb: '1rem'
-      }}
-      onSubmit={createTodo}
-    >
-      <TextField
-        value={name}
-        onChange={handleName}
-        variant="outlined"
-        label="Todo"
-      />
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <DateTimePicker
-          label="Due Date"
-          value={dueDate}
-          onChange={handleDate}
-          renderInput={(params) => <TextField {...params} />}
+    <>
+      <Notification severity={severity} message={message} />
+      <Box
+        component="form"
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: '2fr 1fr auto',
+          columnGap: '0.625rem',
+          mb: '1rem'
+        }}
+        onSubmit={createTodo}
+      >
+        <TextField
+          value={name}
+          onChange={handleName}
+          variant="outlined"
+          label="Todo"
         />
-      </LocalizationProvider>
-      <Button variant="contained" type="submit">
-        create
-      </Button>
-    </Box>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DateTimePicker
+            label="Due Date"
+            value={dueDate}
+            onChange={handleDate}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <Button variant="contained" type="submit">
+          create
+        </Button>
+      </Box>
+    </>
   );
 }
